62b058abfdd16fdfd7e1c7294e639409b2610e88
tathxo <tatheckso@gmail.com>
2025-11-22 04:41:44 -0600

    Add Player and LocationId to ItemReceivedEventArgs
    LocationId was never filled in.
    Player was not previously on it.
    
    For DSAP, I need a combination of both to uniquely match an item to auxiliary info (e.g. weapon upgrade data) in the slot data upon an ItemReceived event.

--- Models/ItemReceivedEventArgs.cs
+++ Models/ItemReceivedEventArgs.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Archipelago.MultiClient.Net.Helpers;
 
 namespace Archipelago.Core.Models
 {
     public class ItemReceivedEventArgs : EventArgs
     {
         public Item Item { get; set; }
-        public int LocationId { get; set; }
+        public long LocationId { get; set; }
+        public PlayerInfo Player { get; set; }
     }
 }
